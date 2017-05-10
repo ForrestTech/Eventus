d77dbc4e131140dede9c6c7a53b301ea563c478f
Richard Forrest <richard.a.forrest@gmail.com>
2017-05-10 09:19:52 +0100

    Create first documentdb test

--- src/EventSourcing.Tests.DocumentDb/Scenario.cs
+++ src/EventSourcing.Tests.DocumentDb/Scenario.cs
@@ -1,4 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using EventSourceDemo.Commands;
+using EventSourceDemo.Domain;
+using EventSourceDemo.Handlers;
+using EventSourcing.Samples.Infrastructure;
+using FluentAssertions;
 using Xunit;
 
 namespace EventSourcing.Tests.Integration
@@ -8,7 +14,28 @@ namespace EventSourcing.Tests.Integration
         [Fact]
         public async Task RunBankingSenario()
         {
-            
+            var cleaner = TearDownFactory.Create();
+            await cleaner.TearDownAsync();
+
+            var accountId = Guid.NewGuid();
+
+            var repo = RepositoryFactory.Create();
+            var handler = new BankAccountCommandHandlers(repo);
+            var joeBloggs = "Joe Bloggs";
+
+            await handler.HandleAsync(new CreateAccountCommand(Guid.NewGuid(), accountId, joeBloggs));
+            await handler.HandleAsync(new DepostiFundsCommand(Guid.NewGuid(), accountId, 10));
+            await handler.HandleAsync(new DepostiFundsCommand(Guid.NewGuid(), accountId, 35));
+            await handler.HandleAsync(new WithdrawFundsCommand(Guid.NewGuid(), accountId, 25));
+            await handler.HandleAsync(new DepostiFundsCommand(Guid.NewGuid(), accountId, 5));
+            await handler.HandleAsync(new WithdrawFundsCommand(Guid.NewGuid(), accountId, 10));
+
+            var actual = await repo.GetByIdAsync<BankAccount>(accountId);
+
+            actual.Id.Should().Be(accountId);
+            actual.Name.Should().Be(joeBloggs);
+            actual.CurrentBalance.Should().Be(15);
+            actual.Transactions.Count.Should().Be(5);
         }
     }
 }
