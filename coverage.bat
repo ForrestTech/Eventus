830161c46f190e3d14935c82f2e1b507750f3fe1
Richard Forrest <feanz@users.noreply.github.com>
2017-05-19 09:00:47 +0100

    Add appveyor (#27)
    resolves #18

--- coverage.bat
+++ coverage.bat
@@ -1,12 +1,6 @@
 @echo off
- .\tools\Opencover\OpenCover.Console.exe -register:user -target:".\tools\xunit\xunit.console.x86.exe" -targetargs:".\src\Eventus.Tests.Unit\bin\Debug\Eventus.Tests.Unit.dll -noshadow" -output:".\build\coverage.xml" -filter:"+[Eventus*]* -[EEventus]Eventus.Properties.* -[Eventus.Tests.Unit*]* -[Eventus.Samples.Core*]*"
+
+ .\tools\Opencover\OpenCover.Console.exe -register:user -target:".\tools\xunit\xunit.console.x86.exe" -targetargs:".\src\Eventus.Tests.Unit\bin\Debug\Eventus.Tests.Unit.dll -noshadow" -output:".\build\coverage.xml" -filter:"+[Eventus*]* -[Eventus]Eventus.Properties.* -[Eventus.Tests.Unit*]* -[Eventus.Samples.Core*]*"
 if %ERRORLEVEL% == 0 goto :next
 
-:quit
-exit /b %ERRORLEVEL%
-
-:next
-@echo.
-@echo %date%
-@echo %time%
-@echo.
+cmd /k
