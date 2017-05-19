81c5f17c6904792d0973449055970d7377bec9ed
Richard Forrest <richard.a.forrest@gmail.com>
2017-05-19 08:21:38 +0100

    Update yml

--- coverage.bat
+++ coverage.bat
@@ -1,6 +1,6 @@
 @echo off
 
- .\tools\Opencover\OpenCover.Console.exe -register:path64 -target:".\tools\xunit\xunit.console.x86.exe" -targetargs:".\src\Eventus.Tests.Unit\bin\Debug\Eventus.Tests.Unit.dll -noshadow" -output:".\build\coverage.xml" -filter:"+[Eventus*]* -[Eventus]Eventus.Properties.* -[Eventus.Tests.Unit*]* -[Eventus.Samples.Core*]*"
+ .\tools\Opencover\OpenCover.Console.exe -register:user -target:".\tools\xunit\xunit.console.x86.exe" -targetargs:".\src\Eventus.Tests.Unit\bin\Debug\Eventus.Tests.Unit.dll -noshadow" -output:".\build\coverage.xml" -filter:"+[Eventus*]* -[Eventus]Eventus.Properties.* -[Eventus.Tests.Unit*]* -[Eventus.Samples.Core*]*"
 if %ERRORLEVEL% == 0 goto :next
 
 cmd /k
