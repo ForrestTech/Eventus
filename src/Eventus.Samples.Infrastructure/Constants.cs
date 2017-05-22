bf7b3ee3212c5c1276a5bca888361012bbf202ec
Richard Forrest <richard.a.forrest@gmail.com>
2017-05-22 11:07:28 +0100

    fixup

--- src/Eventus.Samples.Infrastructure/Constants.cs
+++ src/Eventus.Samples.Infrastructure/Constants.cs
@@ -3,9 +3,5 @@
     public class Constants
     {
         public const string Provider = "Provider";
-
-        public const string Eventstore = "eventstore";
-
-        public const string DocumentDb = "documentdb";
     }
 }
