460836e139c9a1f3645cb96ecce2232c48e539a7
Richard Forrest <richard.a.forrest@gmail.com>
2017-06-07 14:29:33 +0100

    WIP Initial Site

---
+++ src/Eventus.Samples.Web/Startup.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Eventus.Samples.Web.Data;
+using Eventus.Samples.Web.Models;
+using Eventus.Samples.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eventus.Samples.Web
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(config =>
+                {
+                    config.Password.RequireNonAlphanumeric = false;
+                    config.SignIn.RequireConfirmedEmail = true;
+                    config.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+                    config.Lockout.MaxFailedAccessAttempts = 10;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<EmailOptions>(Configuration);
+            services.Configure<SmsOptions>(Configuration);
+
+            services.AddMvc(options =>
+            {
+                options.SslPort = 44374;
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
+
+            // Add application services.
+            services.AddTransient<IEmailSender, MailGunEmailSender>();
+            services.AddTransient<ISmsSender, TwilioSmsSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            app.UseGoogleAuthentication(new GoogleOptions
+            {
+                ClientId = Configuration["GoogleClientID"],
+                ClientSecret = Configuration["GoogleClientSecret"]
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
