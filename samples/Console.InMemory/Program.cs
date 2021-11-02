0505f54e9bd17402bbdeecd54c700fc782023e0c
richard.a.forrest@gmail.com <richard.a.forrest@gmail.com>
2021-11-02 09:42:38 +0700

    Clean up sample

--- samples/Console.InMemory/Program.cs
+++ samples/Console.InMemory/Program.cs
@@ -7,15 +7,7 @@
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
     using System.Threading.Tasks;
-    
-   
-    //TODO github action builds
-    //TODO add sample for event publishing
-    //TODO sort out nuget publishing
-    //TODO possible change to snapshot strategy if we loaded from snapshot we could store the details in the aggregate for writing or just store if we are in the modules
-    //TODO SQL perf improvements single call to DB for a batch of commits
-    //TODO SQL schema migration support
-    //TODO is it better to pass the aggregate to the event so that version setting is controlled???
+
     static class Program
     {
         static async Task Main(string[] args)
